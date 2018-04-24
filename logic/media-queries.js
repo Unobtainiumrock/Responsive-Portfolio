e150f43e815629a9f909807f9c221b50e2fb873a
Unobtainiumrock <unobtainiumrock@gmail.com>
2018-04-23 21:56:43 -0700

    Add Jquery media query for resizing the container class to always be the exact size of the screen

---
+++ logic/media-queries.js
@@ -0,0 +1,13 @@
+
+// Break points between 979 - 767 were bugging out my gradient
+// I think it also did it for 767 - 641 too. 
+// this makes the container size dynamially change to fit whatever screen size.
+$(document).ready(function() {
+  $(window).resize(function(e) {
+    var screenSize = document.body.clientWidth;
+
+    $('.container').css({
+      width: screenSize
+    });
+  })
+});
